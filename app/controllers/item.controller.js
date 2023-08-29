2c2fc0024c2d6c2176ee646ba12c051865809552
JUBILEU404 <locaytonfenix@gmai.com>
2023-08-29 11:12:19 -0300

    adicionado configuração de itens

--- app/controllers/item.controller.js
+++ app/controllers/item.controller.js
@@ -1,5 +1,5 @@
 const  db = require("../models");
-const Item = db.item;
+const Item = db.items;
 const Op = db.Sequelize.Op;
 
 exports.create = (req, res) => {
@@ -14,28 +14,28 @@ exports.create = (req, res) => {
      name: req.body.name,
      description: req.body.description,
      quantity: req.body.quantity,
-     is_flammable: req.body.is_flammable ? req.body.is_flammable : false       
-    }
-};
+     isFlammable: req.body.isFlammable ? req.body.isFlammable : false       
+    };
 
-Item.create(item)
-.then(data => {
+    Item.create(item)
+    .then(data => {
     res.send(data);
-})
+    })
 
 .catch(err => {
     res.status(500).send({
         message:
         err.message || "Ocorreu um erro ao criar o item."
     })
-});
+})
+};
 
 
 exports.findAll = (req, res) => {
     const name = req.body.name;
     var condition = name ? { name: { [Op.like]: `%${name}$%` } } : null;
 
-    items.findAll({ where: condition })
+    Item.findAll({ where: condition })
     .then(data => {
         res.send(data);
     })
@@ -49,15 +49,47 @@ exports.findAll = (req, res) => {
 };
 
 exports.findOne = (req, res) => {
+    const id = req.params.id;
 
+    Item.findByPk(id)
+    .then(data => {
+        if (data){
+            res.send(data);
+        } else {
+            res.status(404).send({
+                messege: `Não foi possivel encontrar um item com o id=${id}.`
+            }); 
+        }
+    })
+    .catch(err =>{
+        res.status(500).send({
+                message: "Ocorreu um erro ao tentar encontrar um item com o id=" + id
+        });
+    });
 };
 
 exports.update = (req, res) => {
+    const id = req.params.id;
 
+
+    Item.update(req.body, {
+        where: {id: id}
+    })
+    .then(num => {
+        if (num == 1) {
+            res.send({
+                message: "O item foi atualizado com sucesso"
+            });
+        } else {
+            res.send({
+                message: `Não foi possivel atualizar o item=${id}.`
+            });
+        }
+    })
 };
 
 exports.delete = (req, res) => {
-
+    
 };
 
 exports.deleteALL = (req, res) => {
